e3168d3526e4dbdece60adf7e613537e150fb48b
Marco Gabrecht <m.gabrecht@gmx.de>
2025-04-03 11:15:29 +0200

    Brownout

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
 #include "TinkerThinkerBoard.h"
 #include "ConfigManager.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
 // Erzeuge globalen Board- und ConfigManager
 ConfigManager configManager;
 TinkerThinkerBoard board(&configManager);
 
 void setup() {
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+
     Serial.begin(115200);
     if (!configManager.init()) {
         Serial.println("Failed to init ConfigManager!");
